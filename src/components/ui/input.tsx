fd0db75acfce332837721a50bd5c35107ac6bb5a
lancetang <lance@cubdigital.co.nz>
2024-10-13 11:41:47 +1300

    fix: minor

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -4,7 +4,7 @@ import { cn } from "~/lib/utils";
 
 export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {
-  type: "text" | "password" | "email" | "number";
+  type?: "text" | "password" | "email" | "number";
 }
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
